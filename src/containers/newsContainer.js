8fed452e47c86214b2d846674aebfaf8be97e194
James <james.m.linklater@gmail.com>
2022-08-10 09:19:11 +0100

    cant remember

--- src/containers/newsContainer.js
+++ src/containers/newsContainer.js
@@ -1,37 +1,28 @@
-import React, {useState, useEffect} from "react";
-import NewsItems from "../components/newsItem";
+import React, { useState, useEffect } from "react";
 import NewsSearch from "../components/filter";
 
 const NewsContainer = () => {
+  const [stories, setStories] = useState([]);
 
-    const [storyId, setStoryId] = useState([])
-    const [stories, setStories] = useState([])
-    const [storyData, setStoryData] = useState([])
+  useEffect(() => {
+    fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
+      .then((res) => res.json())
+      .then((data) => {
+        let newData = data.splice(20, data.length);
+        const storyPromises = newData.map((story) => {
+          return fetch(
+            `https://hacker-news.firebaseio.com/v0/item/${story}.json`
+          ).then((res) => res.json());
+        });
+        Promise.all(storyPromises).then((data) => setStories(data));
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch('https://hacker-news.firebaseio.com/v0/topstories.json').then(res => res.json()).then(data => {
-            data.splice(50, data.length)
-            setStoryId(data)
-        })
-        const storyPromises = storyId.map((story) => {
-            return fetch(`https://hacker-news.firebaseio.com/v0/item/${story}.json`).then(res => res.json())
-        })
-        console.log(storyPromises)
-        Promise.all(storyPromises).then((data) => 
-            setStories(data)
-        )
-    }, [])
+  return (
+    <>
+      <NewsSearch stories={stories} />
+    </>
+  );
+};
 
-    const selectStory = (story) => {
-
-    }
-
-
-    return(<>
-    <NewsSearch stories={stories}/>
-    </>)
-
-
-}
-
-export default NewsContainer
+export default NewsContainer;
